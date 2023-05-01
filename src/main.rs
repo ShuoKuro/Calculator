155fadf777fd238e458b8037b5182639f9b8d8b9
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 13:06:04 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let a: i32 = 0;
+    let b: i32 = 0;
+    let operator: char = '+';
+    match operator {
+        '+' => print!("{}", a + b),
+        '-' => print!("{}", a - b),
+        '/' => {
+            if b != 0 {
+                print!("{}", a / b);
+            } else {
+                print!("Division by 0 is undefined");
+            }
+        }
+        '*' => print!("{}", a * b),
+        '%' => print!("{}", a % b),
+        _ => print!("invalid operator"),
+    };
 }
