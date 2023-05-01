24cb855f6ac8aacad89dd48b4949026caa453dc1
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 13:07:32 +0800

    mod 0 is undefined

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,13 @@ fn main() {
             }
         }
         '*' => print!("{}", a * b),
-        '%' => print!("{}", a % b),
+        '%' => {
+            if b != 0 {
+                print!("{}", a % b);
+            } else {
+                print!("Mod 0 is undefined");
+            }
+        }
         _ => print!("invalid operator"),
     };
 }
